be1b0e1cb2130aa8c06c2869a295a99ab1461bdf
J-Krush <jkrush@pm.me>
2023-06-16 18:26:02 -0400

    feat: centralize capabilites data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,51 +1,135 @@
 // Import images
-import WebImage1 from '../images/web-project-1.jpg';
-import WebImage2 from '../images/web-project-2.jpg';
-import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
+import LayeredWallArt from '../images/cat-layers-top.jpg';
+import FoxLayers from '../images/fox-layers-top.jpg';
+
+import TigerEyes from '../images/tiger-eyes-layers-top.jpg';
+import Waves from '../images/waves-layers-top.jpg';
+
+
+import Furniture from '../images/headboard-installed.jpg';
+
 import UIImage1 from '../images/ui-project-1.jpg';
 import UIImage2 from '../images/ui-project-2.jpg';
+import Image3 from '../images/mobile-project-2.jpg';
 
-export const projectsData = [
+export const capabilitiesTitle = 'capabilities';
+
+export const capabilitiesData = [
 	{
-		id: 1,
-		title: 'Google Health Platform',
-		category: 'Web Application',
-		img: WebImage2,
+		id: 'layered-wall-art',
+		title: 'Layered Wall Art',
+		img: LayeredWallArt,
+		slug: `/${capabilitiesTitle}/layered-wall-art`,
 		ProjectHeader: {
-			title: 'Project Management UI - From Context',
-			publishDate: 'Jul 26, 2021',
-			tags: 'UI / Frontend',
+			title: 'Multi-layered Wall Art',
+			tags: 'Unknown',
 		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: 'Fox Layered',
+				img: FoxLayers,
+				isOriginalDesign: false,
+				linkToDesign: '',
+			},
+			{
+				id: 2,
+				title: 'Tiger Eyes Layered',
+				img: TigerEyes,
+				isOriginalDesign: false,
+				linkToDesign: '',
+			},
+			{
+				id: 3,
+				title: 'Waves Layered',
+				img: Waves,
+				isOriginalDesign: false,
+				linkToDesign: '',
+			},
+		],
+		ProjectInfo: {
+			ConsiderationsTitle: 'Considerations',
+			ConsiderationsDetails: '',
+
+			MaterialsTitle: 'Preferred Materials',
+			MaterialsDetails: 'Plywood, Acrylic',
+
+			ProjectDetailsTitle: 'About this style',
+			ProjectDetails: 'This is some text',
+		}
 	},
 	{
-		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Mobile Application',
-		img: MobileImage2,
+		id: 'furniture',
+		title: 'Furniture & Detailing',
+		img: Furniture,
+		slug: `/${capabilitiesTitle}/furniture`,
+
+		ProjectHeader: {
+			title: 'Furniture & Detailing',
+			tags: 'Unknown',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: 'Headboard Installed',
+				img: Furniture,
+				isOriginalDesign: true,
+				linkToDesign: '',
+			},
+			{
+				id: 2,
+				title: 'Tiger Eyes Layered',
+				img: TigerEyes,
+				isOriginalDesign: false,
+				linkToDesign: '',
+			},
+			{
+				id: 3,
+				title: 'Waves Layered',
+				img: Waves,
+				isOriginalDesign: false,
+				linkToDesign: '',
+			},
+		],
+		ProjectInfo: {
+			ConsiderationsTitle: 'Considerations',
+			ConsiderationsDetails: '',
+
+			MaterialsTitle: 'Preferred Materials',
+			MaterialsDetails: 'Plywood, hardwood',
+
+			ProjectDetailsTitle: 'About this style',
+			ProjectDetails: 'This is some text',
+		}
 	},
 	{
-		id: 3,
-		title: 'Project Management UI',
-		category: 'UI/UX Design',
+		id: '3d-slices',
+		title: '3D Model Slicing',
 		img: UIImage1,
+		slug: `/${capabilitiesTitle}/3d-slices`,
 	},
 	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
+		id: 'signage',
+		title: 'Signage',
 		img: UIImage2,
+		slug: `/${capabilitiesTitle}/signage`,
 	},
 	{
-		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
-		img: MobileImage1,
+		id: 'jewelry',
+		title: 'Jewelry',
+		img: Image3,
+		slug: `/${capabilitiesTitle}/jewelry`,
 	},
 	{
-		id: 6,
-		title: 'Apple Design System',
-		category: 'Web Application',
-		img: WebImage1,
+		id: 'merchandise',
+		title: 'Merchandise & Party Favors',
+		img: Image3,
+		slug: `/${capabilitiesTitle}/merchandise`,
 	},
+	// {
+	// 	id: 7,
+	// 	title: 'Custom Work',
+	// 	img: Image3,
+	// 	slug: `/${projectsTitle}/custom`,
+	// },
 ];
