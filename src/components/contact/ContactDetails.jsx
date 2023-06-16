5b878038f47115a4c86cd68d0269acac0f831e1d
J-Krush <jkrush@pm.me>
2023-06-16 18:26:53 -0400

    refactor: update contact details

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -1,21 +1,16 @@
-import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
+import { FiMapPin, FiMail } from 'react-icons/fi';
 
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Asheville NC',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'jkrush@shapesmith.studio',
 		icon: <FiMail />,
-	},
-	{
-		id: 3,
-		name: '555 8888 888',
-		icon: <FiPhone />,
-	},
+	}
 ];
 
 const ContactDetails = () => {
