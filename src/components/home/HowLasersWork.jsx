ab8fbec16e9b8a967dfc00f0f2a8c18cc54f6783
J-Krush <jkrush@pm.me>
2023-06-26 19:17:58 -0400

    feat: add how lasers work section

---
+++ src/components/home/HowLasersWork.jsx
@@ -0,0 +1,24 @@
+import BedSize from '../../images/bed-size.png';
+
+const HowLasersWork = () => {
+
+    return (
+        <section className="py-5 sm:py-10 mt-5 sm:mt-10">
+				<div className="container mx-auto mt-24">
+					<div className='text-center'>
+						<h1
+							className='font-display font-black text-4xl lg:text-3xl xl:text-4xl md:text-center sm:text-left text-ternary-dark dark:text-primary-light'>
+							How Lasers Work
+						</h1>
+					</div>
+					
+					<p className="font-display font-regular text-m mb-1 text-ternary-dark dark:text-ternary-light">
+
+						C02 Lasers blah blah blah 
+					</p>
+				</div>
+			</section>
+    )
+}
+
+export default HowLasersWork;
