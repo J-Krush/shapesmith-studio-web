7834c1f9a0b11ee3285da3b1a35c1a5a907cc312
J-Krush <jkrush@pm.me>
2023-06-26 19:20:51 -0400

    feat: add new sections to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,31 @@ import AppBanner from '../components/shared/AppBanner';
 import ProjectsGrid from '../components/projects/ProjectsGrid';
 import OurProcess from '../components/home/OurProcess';
 import { ProjectsProvider } from '../context/ProjectsContext';
+import Collaborations from '../components/home/Collaborations';
+import QuickInfo from '../components/home/QuickInfo';
+import QuickSpecs from '../components/home/QuickSpecs';
+import HowLasersWork from '../components/home/HowLasersWork';
 
 const Home = () => {
 	return (
-		// <div className="container mx-auto">
 		<div>
 			<AppBanner></AppBanner>
 
+			<QuickInfo />
+
+			<div className="mt-24">
+				<ProjectsProvider>
+					<ProjectsGrid></ProjectsGrid>
+				</ProjectsProvider>
+			</div>
+
 			<OurProcess />
 
-			<ProjectsProvider>
-				<ProjectsGrid></ProjectsGrid>
-			</ProjectsProvider>
+			<QuickSpecs />
+
+			{/* <HowLasersWork /> */}
+
+			<Collaborations />
 
 		</div>
 	);
