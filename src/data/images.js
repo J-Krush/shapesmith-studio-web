4df4dd2e6dedd8148a2dc0ea041bbcfed99a353e
J-Krush <jkrush@pm.me>
2023-07-13 17:52:37 -0400

    refactor: add images file for tracking drive links

---
+++ src/data/images.js
@@ -0,0 +1,194 @@
+
+
+export const LaserBedSize = {
+    localFile: 'bed-size.png',
+    driveFile: '17Vjd2G_2ayhEWpsG7h3BQynzYAvhf4Hx',
+}
+
+export const BloomSide1 = {
+    localFile: 'bloom-layers-side-1.jpg',
+    driveFile: '1dQlf_BO--1euY5cxXmwY9L8AAKYj1KUR',
+}
+
+export const BloomSide2 = {
+    localFile: 'bloom-layers-side-2.jpg',
+    driveFile: '1gB913L9RJoqMZGdLV_3Q5O2S02qOdlZS',
+}
+
+export const BloomTop = {
+    localFile: 'bloom-layers-top.jpg',
+    driveFile: '19Cvz9MCyl1HPaIzDGyDOBEb4nGSuUEZu',
+}
+
+export const CatTop = {
+    localFile: 'cat-layers-top.jpg',
+    driveFile: '1m6nZrlWUds8K9tHMDB9wPug68Siv2we7',
+}
+
+export const DavilleCampSign = {
+    localFile: 'daville-camp-sign.JPG',
+    driveFile: '1CtnrxxDzXKNqNO7gaDHP9TRH0C2Uwh3q',
+}
+
+export const DavilleDJ = {
+    localFile: 'daville-dj-1.jpg',
+    driveFile: '1Say_JKVOm3wRUtOQOGAmxq3sy_8v4s5k',
+}
+
+export const EarringsBatch1 = {
+    localFile: 'earrings-batch-1.jpg',
+    driveFile: '1BoDyNdP11g5eOy_s5AQ4xFULkPtkEOf2',
+}
+
+export const EarringsBatch2 = {
+    localFile: 'earrings-batch-2.jpg',
+    driveFile: '1E8cbzItLw-eoNHdHNAVYJ0Lf6FwgKrg0',
+}
+
+export const EarringsBatch3 = {
+    localFile: 'earrings-batch-3.jpg',
+    driveFile: '1aVSac6nk6WSkZDbXSMoB1ovQtVso2ti0',
+}
+
+export const EarringsGeometric = {
+    localFile: 'earrings-geometric-close-up.jpg',
+    driveFile: '1c2ry14wAXEtlnDGDko75vG1UQSLJ99uT',
+}
+
+export const Effigy1 = {
+    localFile: 'effigy-1.jpg',
+    driveFile: '1Y7vV9x1LaDYlSfmiQ-c0iVbuh3D7ixao',
+}
+
+export const Effigy3 = {
+    localFile: 'effigy-3.jpg',
+    driveFile: '1wRGsHI_5KOJwpXE50dmR_-SS9Ax16TgP',
+}
+
+export const FlowerOfLifeSide = {
+    localFile: 'flower-of-life-layers-side.JPG',
+    driveFile: '1fdrkrHWYXB8dS5Txszi81V0YTPvp0eRV',
+}
+
+export const FlowerOfLifeTop = {
+    localFile: 'flower-of-life-layers-top.JPG',
+    driveFile: '1SEP66cIQ7pC5MAZ1myRYac1FbWbdFn3h',
+}
+
+export const FoxTop = {
+    localFile: 'fox-layers-top.jpg',
+    driveFile: '1Cpak17njzsRe8GWWJ2F-v80GZ8AKX83C',
+}
+
+export const HeadboardInstalled = {
+    localFile: 'headboard-installed.jpg',
+    driveFile: '1zPD2v2sYRFMfx4n9ka1Ambnyc1RiRbZb',
+}
+
+export const InkscapeScreenshot = {
+    localFile: 'inkscape-screenshot.png',
+    driveFile: '1c4XD5hQfll_g3CIXhea2ZG-QBZH_KcaQ'
+}
+
+export const InwardArtsSigns = {
+    localFile: 'inward-arts-signs.jpg',
+    driveFile: '1qX5-LIA54nmtt7c4T7rChzNh7Gb8p0O5'
+}
+
+export const JKProfile = {
+    localFile: 'JK-profile.PNG',
+    driveFile: '1kCrvKoadNHiqRlOKhyrIFC39s5kP8bQM'
+}
+
+export const LadiesSign = {
+    localFile: 'ladies-sign.jpg',
+    driveFile: '1dnfv-3WDfmprPDKLS3PGyIKGusgzmvvQ'
+}
+
+export const MetatronAssembly = {
+    localFile: 'metatron-assembly.png',
+    driveFile: '1rDdaVscHbdd1A78ECFAy0DzxPcZObi8M'
+}
+
+export const MetatronCutting = {
+    localFile: 'laser-cutting-metatron.png',
+    driveFile: '1vbj1J1fExXBX8Q426Q-4XyOVmMlEWoex'
+}
+
+export const MetatronFinished1 = {
+    localFile: 'metatron-finished-1.jpg',
+    driveFile: '13iO7-NmIvDBqM283b76wTpRKntZ8C233'
+}
+
+export const MetatronFinished2 = {
+    localFile: 'metatron-finished-2.jpg',
+    driveFile: '1N0RItsN60KKL_fbYn127PpZnoRTpx6RA'
+}
+
+export const OversizedPiece = {
+    localFile: 'oversized-piece.jpg',
+    driveFile: '1Pi2dsuyyrePqZiYMpkVLiqZMOXxPjqwG'
+}
+
+export const SkullSliced = {
+    localFile: 'skull-sliced-1.jpg',
+    driveFile: '1TMTn4O8-hu52CXix8kpV2SyLbOKbDjEj',
+}
+
+export const SunriseTop = {
+    localFile: 'sunrise.jpg',
+    driveFile: '1MqY79Bk9auQOLApd04mYBTuBGpzkJ01G',
+}
+
+export const TigerEyesTop = {
+    localFile: 'tiger-eyes-layers-top.jpg',
+    driveFile: '1tjIFZDQzu38dquXUtXFYalSKYZARhpKR',
+}
+
+export const VenmoSign = {
+    localFile: 'venmo-1.jpg',
+    driveFile: '1qy0UMcrZFHX1CvXI99_AfdPG3pdbtHoN',
+}
+
+export const WavesTop = {
+    localFile: 'waves-layers-top-2.jpg',
+    driveFile: '1zJ1YKuGFny3aGN-4rl7Z6MB2UfkIJHkD',
+}
+
+export const WelcomeToTheShire = {
+    localFile: 'welcome-to-the-shire-sign.jpg',
+    driveFile: '1ZPunDESUjJi7Axq9rZ9ESL2MjlLN0sbD',
+}
+
+
+// Materials 
+
+export const PlywoodMaterial = {
+    localFile: '/materials/plywood-2.jpeg',
+    driveFile: '1qFcoEIwPkHSzekisB_J_TZOqokPprnbe',
+}
+
+export const AcrylicMaterial = {
+    localFile: '/materials/acrylic-1.jpeg',
+    driveFile: '14Hyjj4SHWLY9bYF4_fTyBbw-MMrYXGjZ',
+}
+
+export const HardwoodMaterial = {
+    localFile: '/materials/hardwood-1.jpeg',
+    driveFile: '1K7rpvzClExAVbup4OW3yRsmJYhzln-0r',
+}
+
+export const MDFMaterial = {
+    localFile: '/materials/mdf-1.png',
+    driveFile: '1irfuNMriqkEYhKQwORFHIR-PA281l_Br',
+}
+
+export const LeatherMaterial = {
+    localFile: '/materials/leather-2.jpeg',
+    driveFile: '18k7QORL4lhiCs4lgHF2v01zpuv2r-IeH',
+}
+
+export const MetalMaterial = {
+    localFile: '/materials/metal-1.jpeg',
+    driveFile: '1LbObZ5_cVrKD4rwtwcmlkns-sVg1rc4z',
+}
