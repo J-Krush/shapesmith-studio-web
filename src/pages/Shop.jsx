ddad98394db5347864c6be542cb02fb7176fc12b
J-Krush <jkrush@pm.me>
2023-07-13 17:55:28 -0400

    feat: add placeholder shop page

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,17 @@
+
+const Shop = () => {
+
+	return (
+        <section className="py-5 sm:py-10 mt-5 sm:mt-10">
+            <div className="container mx-auto">
+                <div className="text-center">
+                    <p className="capitalize font-display font-bold text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
+                        Shop Coming Soon! 
+                    </p>
+                </div>
+            </div>
+		</section>
+	);
+};
+
+export default Shop;
