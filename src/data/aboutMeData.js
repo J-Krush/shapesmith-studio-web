b62827f6ecda970c5e76ae119aed1cc36329ef83
J-Krush <jkrush@pm.me>
2023-07-13 17:52:22 -0400

    refactor: update bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,7 +1,7 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: `Hi, I'm John Kreisher. Sometimes I go by J-Krush. I wear many hats. I'm an aerospace engineer turned circus performer`,
+		bio: `Hi, I'm John Kreisher (J-Krush). I wear many hats. I'm an aerospace engineer turned circus performer`,
 	},
 	{
 		id: 2,
